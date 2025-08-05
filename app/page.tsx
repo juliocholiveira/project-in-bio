586c3bf397976ba8f94cc1da30723679fa6bc83b
juliocholiveira <juliocholiveira@gmail.com>
2025-08-05 08:10:38 +0000

    nova aula

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,12 @@
+import Hero from "./components/landing-page/hero";
+
 export default function Home() {
-  return <div className="">Hello world 10</div>;
+  <div className="max-w-7xl mx-auto">
+    <Hero />
+
+    {/* <Header />
+      <VideoExplanation />
+      <Pricing />
+      <FAQ /> */}
+  </div>;
 }
--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  allowedDevOrigins: [
+    "cloudworkstations.dev",
+    "*.cloudworkstations.dev",
+    "3000-firebase-project-in-biogit-1753198900720.cluster-duylic2g3fbzerqpzxxbw6helm.cloudworkstations.dev",
+  ],
 };
 
 export default nextConfig;
